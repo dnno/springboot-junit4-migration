80a31d2683358596e32111f22257648fff9e4b20
Reinhard Prechtl <reinhard.prechtl@codecentric.de>
2017-09-14 15:16:58 +0200

    Migrate integration test using JUnitPlatform runner

--- src/test/java/de/rpr/demowebapp/DemoWebappApplicationIntegrationTest.java
+++ src/test/java/de/rpr/demowebapp/DemoWebappApplicationIntegrationTest.java
@@ -1,16 +1,19 @@
 package de.rpr.demowebapp;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@RunWith(JUnitPlatform.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-public class DemoWebappApplicationIntegrationTest {
+class DemoWebappApplicationIntegrationTest {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
